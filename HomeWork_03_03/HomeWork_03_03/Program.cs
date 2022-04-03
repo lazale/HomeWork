1fc0773efb1c2109af56c5b29588cdae755159d0
lazale <afif07@mail.ru>
2022-04-03 22:02:16 +0300

    HomeWork_03_03

---
+++ HomeWork_03_03/HomeWork_03_03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWork_03_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число: ");
+            int number = int.Parse(Console.ReadLine());
+            int i = 2;
+            bool numberPrime = true;
+
+
+            while (i < number)
+            {
+                if (number % i == 0)
+                {
+                    numberPrime = true;
+                    break;
+                }
+                else
+                    numberPrime = false;
+                i++;
+            }
+
+            if (numberPrime == false)
+            {
+                Console.WriteLine("Это простое число");
+            }
+            else
+            {
+                Console.WriteLine("Это составное число");
+            }
+        }
+    }
+}
