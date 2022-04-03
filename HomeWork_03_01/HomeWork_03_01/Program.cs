d62e713889bd0559dcbd3a62eaf3dbaea575051e
lazale <afif07@mail.ru>
2022-04-03 22:00:06 +0300

    HomeWork_03_01

---
+++ HomeWork_03_01/HomeWork_03_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeWork_03_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbUser;
+
+            Console.WriteLine("Введите целое число: ");
+            numbUser = int.Parse(Console.ReadLine());
+
+            if (numbUser % 2 == 0)
+            {
+                Console.WriteLine("Вы ввели чётное число");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели нечётное число");
+            }
+        }
+    }
+}
