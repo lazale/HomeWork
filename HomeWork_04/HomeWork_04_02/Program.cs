de6966bc04c547d8b251272cdf3e6836a34df9c0
lazale <afif07@mail.ru>
2022-04-12 00:32:25 +0300

    HomeWork_04_02

---
+++ HomeWork_04/HomeWork_04_02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork_04_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                Console.WriteLine("Введите длину последовательности: ");
+                int dlina = int.Parse(Console.ReadLine());
+                int[] array = new int[dlina];
+                int count = 1;
+
+
+                Console.WriteLine("Введите целые числа поочередно.");
+
+                for (int i = 0; i < array.Length; i++)
+                {   
+                    Console.WriteLine($"Введите число {count++}:");
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+
+                int min = int.MaxValue;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] < min)
+                    { min = array[i]; }
+
+                }
+
+                Console.WriteLine($"Вы ввели самое наименьшее число: {min};");
+            }
+        }
+    }
+}
