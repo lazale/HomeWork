72c8fd6a443edd715c290014710c6b8611802170
lazale <afif07@mail.ru>
2022-04-12 00:20:13 +0300

    HomeWork_04_01

--- HomeWork_04/HomeWork_04_01/Program.cs
+++ HomeWork_04/HomeWork_04_01/Program.cs
@@ -6,32 +6,31 @@ namespace HomeWork_04_01
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество строк: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            int[,] arr2d = new int[row, col];
+
+            Random rand = new Random();
+
+
+            for (int i = 0; i < row; i++)
             {
-                Console.WriteLine("Введите длину последовательности: ");
-                int dlina = int.Parse(Console.ReadLine());
-                int[] array = new int[dlina];
-
-
-
-                Console.WriteLine("Введите целые числа поочередно.");
-
-                for (int i = 0; i < array.Length; i++)
+                for (int j = 0; j < col; j++)
                 {
-                    Console.WriteLine("Введите число:");
-                    array[i] = int.Parse(Console.ReadLine());
+                    arr2d[i, j] = rand.Next(1, 10);
+                    Console.Write($"{arr2d[i, j]} ");
+                    sum += arr2d[i, j];
                 }
-
-                int min = int.MaxValue;
-
-                for (int i = 0; i < array.Length; i++)
-                {
-                    if (array[i] < min)
-                    { min = array[i]; }
-
-                }
-
-                Console.WriteLine($"Вы ввели самое наименьшее число: {min};");
+                Console.WriteLine();
             }
+
+            Console.WriteLine($"Cумма всех элементов матрицы: {sum};");
         }
+    
     }
 }
