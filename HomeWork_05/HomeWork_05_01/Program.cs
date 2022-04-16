5a2d3a71b2fce93d06c73ca6f6ae0847879ddf6d
lazale <afif07@mail.ru>
2022-04-17 00:40:07 +0300

    HomeWork_05_01

---
+++ HomeWork_05/HomeWork_05_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeWork_05_01
+{
+    internal class Program
+    {
+        static String[] TextUserArray(string userText1)
+        {
+            return userText1.Split(' ');
+        }
+        static void TextWordsPrint(string userText2)
+        {
+            foreach (String s in TextUserArray(userText2))
+            {
+                Console.WriteLine(s);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинное предложение разделив слова - пробелом: ");
+            string userText = Console.ReadLine();
+            TextUserArray(userText);
+            TextWordsPrint(userText);
+        }
+
+    }
+}
