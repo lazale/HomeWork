5060d161ce67e2c90f454c35ec69f0943101a2a7
lazale <afif07@mail.ru>
2022-04-17 00:42:40 +0300

    HomeWork_05_02

---
+++ HomeWork_05/HomeWork_05_02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HomeWork_05_02
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Полученный текст разделяется на слова
+        /// </summary>
+        /// <returns></returns>
+        static String[] TextUserArray()
+        {
+            Console.WriteLine("Введите длинное предложение разделив слова - пробелом: ");
+            string userText = Console.ReadLine();
+            string[] arrayUserText = userText.Split(' ');
+            return arrayUserText;
+        }
+        /// <summary>
+        /// Инвертирование слов и их вывод
+        /// </summary>
+        /// <param name="array"></param>
+        static void TextReverse(Array array)
+        {
+            Array.Reverse(array);
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            TextReverse(TextUserArray());
+        }
+    }
+}
