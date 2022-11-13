06e21201c604df57729d5fecea32564e41bca3ef
lazale <afif07@mail.ru>
2022-11-13 04:00:41 +0300

    е2 message, hello, contact, map, tel, video

--- Bots/BotsBeautyTg/Program.cs
+++ Bots/BotsBeautyTg/Program.cs
@@ -16,10 +16,12 @@ namespace ConsoleApp1
             var client = new TelegramBotClient("5743172258:AAFAp0TskCZK8NwURic2jEbiDpplg8jNiK0");
             client.StartReceiving(Update, Error);
             Console.ReadLine();
+
         }
         async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken arg3)
         {
             var message = update.Message;
+
             if (message.Text != null)
             {
                 Console.WriteLine($"{message.Chat.FirstName} | {message.Text}" +
